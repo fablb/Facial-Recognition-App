9c716f10e82b3db08d6f2593a1a8c2b30b282fce
le-bec <fabien.le-bec@ecole.ensicaen.fr>
2022-05-19 11:56:33 +0200

    Initial commit

---
+++ app/src/main/java/com/ensicaen/facialdetectionapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ensicaen.facialdetectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
