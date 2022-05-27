6dc594b21433840a80ce6ccdae58c5bd0133430a
Tony Pinsel-Lampécinado <pinsel.tony@gmail.com>
2022-05-27 14:12:28 +0200

    bdd research by name

--- app/src/main/java/com/ensicaen/facialdetectionapp/model/LocalAccessBDD.java
+++ app/src/main/java/com/ensicaen/facialdetectionapp/model/LocalAccessBDD.java
@@ -9,6 +9,7 @@ import com.ensicaen.facialdetectionapp.utils.MySQLiteOpenHelper;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -27,9 +28,24 @@ public class LocalAccessBDD {
     public void add(Profil profil) {
         _bd = _bdAccess.getWritableDatabase();
         String req = "INSERT INTO profil(name, features, registred_date) VALUES ";
-        req += "(\'"+ profil.get_name() +"\',\'"+ profil.get_features() +"\',\'"+ profil.get_registedDate() +"\')";
-        //Log.e(profil.get_name()+" "+profil.get_features()+" "+profil.get_registedDate());
+        req += "(\'"+ profil.get_name() +"\',\'"+ profil.convertFeaturesToString() +"\',\'"+ profil.get_date() +"\')";
         _bd.execSQL(req);
     }
 
+    public Profil[] searchByName(String name) {
+        String p_name = "", p_features = "";
+        _bd = _bdAccess.getReadableDatabase();
+        String req = "SELECT * FROM profil WHERE name = \'" + name + "\'";
+        Cursor cursor = _bd.rawQuery(req, null);
+        Profil[] profils = new Profil[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            p_name = cursor.getString(1);
+            p_features = cursor.getString(2);
+            profils[i] = new Profil(p_name, p_features, new Date());
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return profils;
+    }
 }
