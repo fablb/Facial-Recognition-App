e50414711da1e6dcd9a8169257d3d5d1e8246ace
Tony Pinsel-Lampécinado <pinsel.tony@gmail.com>
2022-06-24 16:10:04 +0200

    clignement

--- app/src/main/java/com/ensicaen/facialdetectionapp/controller/FaceAcquisitionListener.java
+++ app/src/main/java/com/ensicaen/facialdetectionapp/controller/FaceAcquisitionListener.java
@@ -25,12 +25,16 @@ public class FaceAcquisitionListener extends FaceListener {
     private CameraView _cameraView;
     private SizedArrayList<Point2D> _faceBoundsCenter;
     private LBP _lbp;
+    private float _rightEyeLenghtMax;
+    private float _leftEyeLenghtMax;
 
     public FaceAcquisitionListener(FrameListener drawListener, CameraView cameraView) {
         _drawListener = drawListener;
         _cameraView = cameraView;
         _faceBoundsCenter = new SizedArrayList<>(STABLE_SCREEN_FRAME_COUNT);
         _lbp = new LBP();
+        _rightEyeLenghtMax = 0;
+        _leftEyeLenghtMax = 0;
     }
 
     @SuppressLint("UnsafeOptInUsageError")
@@ -87,6 +91,17 @@ public class FaceAcquisitionListener extends FaceListener {
             List<PointF> rightEye = face.getContour(FaceContour.RIGHT_EYE).getPoints();
             List<PointF> leftEye = face.getContour(FaceContour.LEFT_EYE).getPoints();
 
+            if (((rightEye.get(12).y-rightEye.get(4).y) < _rightEyeLenghtMax*0.60f) ||((leftEye.get(12).y-leftEye.get(4).y) < _leftEyeLenghtMax*0.60f)) {
+                Log.d("1", "T'AS CLIGNE MEC "+String.valueOf(leftEye.get(12).y-leftEye.get(4).y)+" "+String.valueOf(rightEye.get(12).y-rightEye.get(4).y));
+                Log.d("1", String.valueOf(_leftEyeLenghtMax)+" "+String.valueOf(_rightEyeLenghtMax));
+            }
+            if ((rightEye.get(12).y-rightEye.get(4).y) > _rightEyeLenghtMax) {
+                _rightEyeLenghtMax = rightEye.get(12).y-rightEye.get(4).y;
+            }
+            if ((leftEye.get(12).y-leftEye.get(4).y) > _leftEyeLenghtMax) {
+                _leftEyeLenghtMax = leftEye.get(12).y-leftEye.get(4).y;
+            }
+
             if(!eyesOpen(rightEye, leftEye)) {
                 SingleToast.show(_cameraView, "Eyes closed", Toast.LENGTH_SHORT);
                 _drawListener.drawCenterBounds(centerBounds, Color.RED);
@@ -102,7 +117,7 @@ public class FaceAcquisitionListener extends FaceListener {
             SingleToast.clear();
             Bitmap cropBitmap = BitmapUtils.getCropBitmap(_frameProxy, bounds);
             Bitmap scaledBitmap = Bitmap.createScaledBitmap(cropBitmap, 140, 140, true);
-            _cameraView.close(_lbp.compute(scaledBitmap));
+            //_cameraView.close(_lbp.compute(scaledBitmap));
 
             //Log.i("FaceDetectionApp", p.get_name() + " " + p.get_date() + " " + Arrays.toString(p.get_features()));
             //Profile b = db.searchByName("Fabien")[0];
